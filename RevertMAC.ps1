206aa21e1fe9392cb3a1cb50cae470ab0e408083
Wayne Zimmerman <14986626+waynezim@users.noreply.github.com>
2019-10-08 10:02:30 -0500

    Update Adapter Finding Logic

--- RevertMAC.ps1
+++ RevertMAC.ps1
@@ -1,7 +1,13 @@
-. .\Config.ps1
+$NetworkAdapterName = Get-NetAdapter | Where-Object Name -like '*Wi-Fi*' | Select-Object -expand Name
 
-[int]$InterfaceIndex = (Get-NetAdapter | Where-Object Name -eq $NetworkAdapterName).InterfaceIndex
+$InterfaceName = (Get-NetAdapter | Where-Object Name -eq $NetworkAdapterName).InterfaceDescription
 
-[string]$InterfaceIndexReg = "{0:d4}" -f ($InterfaceIndex-1)
+$List = Get-ChildItem "HKLM:\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}" -ErrorAction SilentlyContinue | Select-Object pspath
 
-Remove-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\$InterfaceIndexReg" -Name 'NetworkAddress' -Force
+$AdapterRegPath = $List | Get-ItemProperty -Name DriverDesc -ErrorAction SilentlyContinue | Where-Object DriverDesc -eq $InterfaceName | Select-Object -expand pspath
+
+if ($null -eq $AdapterRegPath){
+    Throw "Failed to translate $NetworkAdapterName to DriverDesc in registry"
+}
+
+Remove-ItemProperty -Path $AdapterRegPath -Name 'NetworkAddress' -Force
